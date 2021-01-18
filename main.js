8847c703a5c87b016b7bed583f2237db03feab4c
Robin <robinplatz@gmail.com>
2021-01-19 00:34:12 +0100

    bot can log in

---
+++ main.js
@@ -0,0 +1,9 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.once('ready', () =>{
+    console.log('SniffBot is online');
+});
+
+
+client.login('ODAwODY0OTMyNDI1MTA1NDA4.YAYVlw.b9HLnnYB2-LlSGmOBo9YmIA0IJs');
