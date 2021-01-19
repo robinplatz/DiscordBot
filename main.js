3da97814334c1f35a102514ea3e406c127b9d35b
Robin <robinplatz@gmail.com>
2021-01-19 14:11:11 +0100

    command recognition

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+const prefix = '!';
+
 client.once('ready', () =>{
     console.log('SniffBot is online');
 });
 
+
+client.on('message', message =>{
+    if(!message.content.startsWith(prefix) || message.author.bot) return;
+
+    
+});
 
 client.login('ODAwODY0OTMyNDI1MTA1NDA4.YAYVlw.b9HLnnYB2-LlSGmOBo9YmIA0IJs');
